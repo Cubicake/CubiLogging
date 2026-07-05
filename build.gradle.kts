a7ee9680699efc20d398153c6e3fcc5a78f42fe0
Cubicake <74905@stpeters.sa.edu.au>
2026-07-05 21:42:21 +0930

    Mark git properties as inputs

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ tasks {
             "build_time" to buildTime.get(),
             "version" to version.toString()
         )
+        inputs.properties(gitProps)
         // build-properties is not used by the logging library itself, but is included in the jar so that platforms can access it.
         filesMatching("build-properties/logging.yml") {
             expand(gitProps)
